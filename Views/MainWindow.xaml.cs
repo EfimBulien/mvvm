bde8ffaedd646c6419bcd2347bbfed66780695da
Ефим Бульен <143902970+EfimBulien@users.noreply.github.com>
2024-04-16 23:45:56 +0300

    fist commit

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using generator.ViewModels;
+using System.Windows;
+
+namespace generator.Views
+{
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
